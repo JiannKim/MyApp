c80a9981271b93f4428660adc114bcf7223a3d6b
JiannKim <jiahnkimm@gmail.com>
2019-10-23 23:09:41 +0900

    accountingApp
    주석추가

--- AccountingApp.java
+++ AccountingApp.java
@@ -15,13 +15,13 @@ public class AccountingApp {
         double dividend2 = income * 0.3;
         double dividend3 = income * 0.2;
  
-        System.out.println("Value of supply : " + valueOfSupply);
-        System.out.println("VAT : " + vat);
-        System.out.println("Total : " + total);
-        System.out.println("Expense : " + expense);
-        System.out.println("Income : " + income);
-        System.out.println("Dividend 1 : " + dividend1);
-        System.out.println("Dividend 2 : " + dividend2);
+        System.out.println("Value of supply : " + valueOfSupply); //공급가
+        System.out.println("VAT : " + vat); //부가세
+        System.out.println("Total : " + total); //판매가
+        System.out.println("Expense : " + expense); //비용
+        System.out.println("Income : " + income); //이익
+        System.out.println("Dividend 1 : " + dividend1); //1투자자배당율
+        System.out.println("Dividend 2 : " + dividend2); //valueOfSupply - expense * 0.3
         System.out.println("Dividend 3 : " + dividend3);
  
     }
